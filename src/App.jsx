6fcefcb2983d666673e80df135dce5be6d0f7d62
Welberth <welbertholiveira250@gmail.com>
2025-11-05 09:40:57 -0300

    feat: tela de ranking de alunos de acordo com pontuação total

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css"
 import Login from "./pages/Login"
 import DashboardAluno from "./pages/DashboardAluno"
 import Atividades from "./pages/Atividades"
+import RankingAlunos from "./pages/RankingAlunos"
 import SplashScreen from "./components/SplashScreen"
 
 function App() {
@@ -28,6 +29,10 @@ function App() {
   if (isLoggedIn) {
     if (currentPage === "atividades") {
       return <Atividades onNavigate={setCurrentPage} onLogout={handleLogout} />
+    }
+    else if (currentPage == "ranking")
+    {
+      return <RankingAlunos onNavigate={setCurrentPage} onLogout={handleLogout}/>
     }
     return <DashboardAluno onNavigate={setCurrentPage} onLogout={handleLogout} />
   } else {
